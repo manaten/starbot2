18fde9b957f1b73a5e4201a9faeb8d5533a3e437
manaten <manaten@manaten.net>
2019-03-19 13:54:42 +0900

    fix(expire): extend expire time.

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const processMessage = async message => {
 };
 
 
-const EXPIRE_MSEC = 60 * 60 * 1000;
+const EXPIRE_MSEC = 7 * 24 * 60 * 60 * 1000;
 const sentMessages = {};
 const run = async isDry => {
   try {
